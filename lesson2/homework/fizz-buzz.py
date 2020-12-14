382a9592294812823adfb3e6753f36d4977df5e2
Пользователь <dusket232888@gmail.com>
2020-12-14 19:53:11 +0200

    testDZ

--- lesson2/homework/fizz-buzz.py
+++ lesson2/homework/fizz-buzz.py
@@ -15,20 +15,17 @@
 Вывод должен быть таким:
 1 F 3 F B F 7 F 9 FB 11 F 13 F B F 17 F
 '''
-fizz = int(input('\nInput \'fizz\': '))
-buzz = int(input('Input \'buzz\': '))
-third = int(input('And now the third one: '))
-
-output = ''
-
-for i in range(1, third + 1):
-    result = ''
-    if not i % fizz:
-        result += 'F'
-    if not i % buzz:
-        result += 'B'
-    if not result:
-        result = i
-    output += f'{result} '
-
-print(f'\n The result is:\n {output}\n')
+fizz = int(input())
+buzz = int(input())
+fibu = int(input())
+i = 1
+while i <= fibu:
+    if((i % fizz == 0) and (i % buzz == 0)):
+        print('FB')
+    elif(i % buzz == 0):
+        print('B')
+    elif(i % fizz == 0):
+        print('F')
+    else:
+        print(i)
+    i = i + 1
